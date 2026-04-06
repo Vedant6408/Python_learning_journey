3535f12ac41d56329e10f88030d7f4b82182898a
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-07 04:21:16 +0530

    nothing changed

--- Bro_Code/main.py
+++ Bro_Code/main.py
@@ -10,12 +10,6 @@ print(f"and your email id is {c}")
 student_of_class = True
 if (student_of_class):
     print("You're a student")
-# line 14 se 17 tak jo vi code likha hai ye mera ek 
-# gendu friend (kishan) bola thaa likhne ke liye 
-# elif if:
-    # print("gendu kishan")
-# if elif:
-    # print(f"gendu kishan")
 else:
     print("You're NOT a student")
 
