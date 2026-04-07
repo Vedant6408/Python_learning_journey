6320bdc7bf3054fc313dfe31c71dd7b183d5a259
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-08 00:48:54 +0530

    Compound interest calculator

---
+++ Bro_Code/6_compound_interest_calc.py
@@ -0,0 +1,28 @@
+#python compound interest calculator using while loop
+#A = P(1 + r/n)t
+#where, A = final amount, P = initial principal balance, r = interest rate & t = time period
+principle = 0
+time = 0
+rate = 0
+
+while principle <= 0: #we had already store principle value to 0 that's why if we try manipulate the condition 
+#to (principle < 0 ) then it already false and from now this loop will not gonna run bcz it's already false
+# if we want to make a program like to enter zero as a principle value then we've to change the condition to
+# while True: 
+    principle = float(input("Enter the principle amount: "))
+    if principle <= 0:
+        print("principle amount can't be less than or equal to zero ")
+while rate <= 0:
+    rate= float(input("Enter the interest rate: "))
+    if rate <= 0:
+        print("interest rate can't be less than or equal to zero ")
+while time <= 0:
+    time = int(input("Enter the Time period (in years): "))
+    if time <= 0:
+        print("time period can't be less than or equal to zero ")
+
+total_balance = principle*(1 + rate/100)**time #we can use both (**)or(pow())
+print(f"Your final balance is: ${total_balance:,}")
+# print(f"{principle:,}")
+# print(rate)
+# print(time)
