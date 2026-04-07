f32b491b1c97394f782a6bddaaa51b4c46cd8593
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-07 23:47:28 +0530

    while loop tut

--- Bro_Code/5_while_loop1.py
+++ Bro_Code/5_while_loop1.py
@@ -1,6 +1,15 @@
 #this is gonna be the first tutorials about while loop programs
-name = input("Enter your name: ")
-while name == "":
-    print("you didn't enter your name")
-    name = input("Enter your name: ")
-print(f"Hey! {name}")
+# name = input("Enter your name: ")
+# while name == "":
+#     print("you didn't enter your name")
+#     name = input("Enter your name: ")
+# print(f"Hey! {name}")
+
+age = int(input('Enter your age: '))
+while age < 0:
+    print("Age can't be in negative")
+    age  = int(input("Enter your age: "))
+if age > 100:
+        print(f"a human being can't surive for 100 years normally. and you said you're {age} years old that's crazy man")
+else:
+     print(f"Your are {age} years old.")
