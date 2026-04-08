1dd200d8c56a186f6e009091a250aff3aa3c9bdc
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-09 00:35:52 +0530

    new day new question solved

---
+++ Logic_building_questions/Day_2/no(day)inmonthprog.py
@@ -0,0 +1,6 @@
+#number of day printing in month
+print("How many days you wanna calculate ?")
+usr_inp = int(input("Enter number of months:: "))
+print(f"The number of days in {usr_inp} months is: {usr_inp*30}")
+print("There are also 28/29, 31 days in month but we take 30 days.")
+#it is simple program we can do more complex coding for this simple program
