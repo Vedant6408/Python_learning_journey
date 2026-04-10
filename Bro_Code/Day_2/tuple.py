cf90487b88b59299a08fa6545dd2a40b836fcd85
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-10 20:39:34 +0530

    tuple basic tut. completed

---
+++ Bro_Code/Day_2/tuple.py
@@ -0,0 +1,11 @@
+#tuple do same like list & set but there are something different from that collection method
+#tuple is immutable
+#tuple is faster 
+#tuple allow to duplicate the value
+tup = (1,2,4,2,2,4,5,"cat","dog")
+# print(tup)
+# print(tup.count())
+# print(len(tup))
+# print(tup.index())
+
+
