9dc3af9261e01647b010774e80a55f5f97420a2c
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-10 20:33:00 +0530

    set tut completed

---
+++ Bro_Code/Day_2/sets.py
@@ -0,0 +1,13 @@
+#set is just like a list but there are something different from list
+#In sets, repeating is not allowed
+set1 = {1,2,4,4,5,6,"Cat", "Dog"}
+# set1.add("tuto")
+# print(help(set1))
+# print(set1.clear())
+# set1.discard("Cat")
+# print(set1) # discard function will remove the element, but not through the error if the element isn't present in the set
+# set1.discard("Elephant")
+# print(set1)
+# set1.difference()
+# print(set1)
+#There are many function, it is not necessary learn it 
