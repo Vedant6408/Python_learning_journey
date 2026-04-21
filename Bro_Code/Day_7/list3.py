fee21d5bbcd4c47342b12f3164ae7248c33ee230
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-21 19:13:16 +0530

    day7

---
+++ Bro_Code/Day_7/list3.py
@@ -0,0 +1,9 @@
+groceries = [ 
+    ["Sprite","Thums-Up","Coca-cola","Pepsi","Mazza"],
+    ["Realme","Samsung","Xioami","Apple","Asus"]
+]
+# print(groceries)
+for a in groceries:
+    for j in a:
+        print(f"___{j}")
+
